e46df324fd5d624eeb27533133ea8a4f1db82ac9
MRSantos1 <martin.r.santos.1@outlook.com>
2020-04-08 15:38:00 -0700

    Adding Jupyter Notebook, initiating analysis

--- covid.py
+++
@@ -1,9 +0,0 @@
-# Import libraries
-import numpy as np
-import pandas as pd
-import plotly as py
-import plotly.express as px
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-init_notebook_mode(connected=True)
